cadcc949e1fe31c2644348d970bf821e9954df38
GeoJulien <julien.moura+dev@gmail.com>
2016-11-29 17:01:25 +0100

    add API strings to be translated (fast and temporal workaround)

---
+++ modules/isogeo_api_strings.py
@@ -0,0 +1,68 @@
+# -*- coding: UTF-8 -*-
+#!/usr/bin/env python
+from __future__ import (absolute_import, unicode_literals)
+# ----------------------------------------------------------------------------
+
+"""
+Additionnal strings to be translated in the exports.
+"""
+
+# Created:      18/10/2016
+# Updated:      22/11/2016
+# ---------------------------------------------------------------------------
+
+dict_md_fields_fr = {
+    "restrictions": {
+    "none": " ",
+        "copyright": "Copyright",
+        "patent": "Brevet",
+        "patentPending": "Brevet en attente",
+        "trademark": "Marque déposée",
+        "license": "Licence",
+        "intellectualPropertyRights": "Droits de propriété intellectuelle",
+        "restricted": "Limité",
+        "other": "Autre"
+        },
+    "constraintType": {
+        "none": " ",
+        "access": "Accès",
+        "usage": "Usage"
+        },
+    "formatTypes": {
+        "vector": "Vecteur",
+        "raster": "Raster",
+        "series": "Ensemble de données"
+        },
+    "roles": {
+        "author": "Auteur",
+        "pointOfContact": "Point de contact",
+        "custodian": "Administrateur",
+        "distributor": "Distributeur",
+        "originator": "Créateur",
+        "owner": "Propriétaire",
+        "principalInvestigator": "Analyste principal",
+        "processor": "Responsable du traitement",
+        "publisher": "Éditeur (publication)",
+        "resourceProvider": "Fournisseur",
+        "user": "Utilisateur"
+        },
+    "frequencyUpdateHelp": "Tous les ",
+    "frequencyTypes": {
+        "years": "an(s)",
+        "months": "mois",
+        "weeks": "semaine(s)",
+        "days": "jour(s)",
+        "hours": "heure(s)",
+        "minutes": "minute(s)",
+        "seconds": "seconde(s)"
+        },
+    "events": {
+        "update": "Mise à jour",
+        "creation": "Création",
+        "published": "Publication"
+    }
+}
+
+
+# print(dict_md_fields_fr.get("roles"))
+# print(dict_md_fields_fr.get("frequencyTypes"))
