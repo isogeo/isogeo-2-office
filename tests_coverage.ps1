cc1b9d798dd38f214f12ba73e3a8322b5cfe6f13
Guts <Guts@users.noreply.github.com>
2018-04-03 12:06:12 +0200

    tests coverage

---
+++ tests_coverage.ps1
@@ -0,0 +1,25 @@
+#################################################################
+#
+# Script to package and upload Isogeo Python package.
+#
+#################################################################
+
+"-- STEP -- Creating virtualenv"
+py -3 -m venv env3_tests
+./env3_tests/Scripts/activate
+
+"-- STEP -- Install and display dependencies within the virtualenv"
+python -m pip install -U pip
+pip install --upgrade setuptools wheel
+pip install --upgrade -r .\requirements.txt
+pip install --upgrade -r .\tests\requirements_test.txt
+
+"-- STEP -- Run coverage"
+coverage run -m unittest discover -s tests/
+
+"-- STEP -- Build and open coverage report"
+coverage html
+Invoke-Item htmlcov/index.html
+
+"-- STEP -- Exit virtualenv"
+deactivate
