9573b0f5d6b6b362a9846a76f05b9b68cd9e3a50
Guts <Guts@users.noreply.github.com>
2018-06-22 11:48:37 +0200

    testing export to xlsx #13

--- tests/test_export_xlsx.py
+++ tests/test_export_xlsx.py
@@ -8,6 +8,7 @@
 # Standard library
 import json
 from os import environ, path
+from tempfile import mkstemp
 import unittest
 
 # 3rd party
@@ -39,14 +40,27 @@ class TestExportXLSX(unittest.TestCase):
     def setUp(self):
         """Executed before each test."""
         # API response samples
-        self.tags_sample = path.normpath(
-            r"tests/fixtures/api_response_tests_tags.json")
+        self.tags_sample_types_all = path.normpath(
+            r"tests/fixtures/api_response_tests_tags_types_all.json")
+        self.tags_sample_types_noRaster = path.normpath(
+            r"tests/fixtures/api_response_tests_tags_types_noRaster.json")
+        self.tags_sample_types_noVector = path.normpath(
+            r"tests/fixtures/api_response_tests_tags_types_noVector.json")
+        self.tags_sample_types_noService = path.normpath(
+            r"tests/fixtures/api_response_tests_tags_types_noService.json")
+        self.tags_sample_types_noResource = path.normpath(
+            r"tests/fixtures/api_response_tests_tags_types_noResource.json")
+        self.search_all_includes = path.normpath(
+            r"tests/fixtures/api_search_complete.json")
+
+        # target class instanciation
         self.out_wb = Isogeo2xlsx(lang="FR", url_base="https://open.isogeo.com/s")
 
     def tearDown(self):
         """Executed after each test."""
         pass
 
+    # -- Workbook structure ---------------------------------------------------
     def test_workbook_subclass(self):
         """Test module subclass."""
         self.assertIsInstance(self.out_wb, Workbook)
@@ -86,7 +100,7 @@ class TestExportXLSX(unittest.TestCase):
     def test_output_structure_all_types_basic(self):
         """Test output workbook worksheets with all types tags."""
         # load tags fixtures
-        with open(self.tags_sample, "r") as f:
+        with open(self.tags_sample_types_all, "r") as f:
             search = json.loads(f.read())
         # run
         self.out_wb.set_worksheets(auto=search.get("tags").keys())
@@ -99,7 +113,7 @@ class TestExportXLSX(unittest.TestCase):
     def test_output_structure_all_types_attributes(self):
         """Test output workbook worksheets with all types tags and attributes."""
         # load tags fixtures
-        with open(self.tags_sample, "r") as f:
+        with open(self.tags_sample_types_all, "r") as f:
             search = json.loads(f.read())
         # run
         self.out_wb.set_worksheets(auto=search.get("tags").keys(), attributes=1)
@@ -110,8 +124,120 @@ class TestExportXLSX(unittest.TestCase):
         self.assertIn("Vecteurs", self.out_wb.sheetnames)
         self.assertIn("Attributs", self.out_wb.sheetnames)
 
+    def test_output_structure_all_types_options(self):
+        """Test output workbook worksheets with all types tags and other options."""
+        # load tags fixtures
+        with open(self.tags_sample_types_all, "r") as f:
+            search = json.loads(f.read())
+        # run
+        self.out_wb.set_worksheets(
+            auto=search.get("tags").keys(), attributes=1,
+            inspire=1, dashboard=1, fillfull=1)
+        print(self.out_wb.sheetnames)
+        self.assertEqual(len(self.out_wb.worksheets), 8)
+        self.assertIn("Raster", self.out_wb.sheetnames)
+        self.assertIn("Services", self.out_wb.sheetnames)
+        self.assertIn("Ressources", self.out_wb.sheetnames)
+        self.assertIn("Vecteurs", self.out_wb.sheetnames)
+        self.assertIn("Attributs", self.out_wb.sheetnames)
+        self.assertIn("Tableau de bord", self.out_wb.sheetnames)
+        self.assertIn("Progression catalogage", self.out_wb.sheetnames)
 
-# ##############################################################################
+    def test_output_structure_noVector_attributes_bad(self):
+        """Test output workbook worksheets with all types tags."""
+        # load tags fixtures
+        with open(self.tags_sample_types_noVector, "r") as f:
+            search = json.loads(f.read())
+        # run
+        self.out_wb.set_worksheets(auto=search.get("tags").keys(), attributes=1)
+        self.assertEqual(len(self.out_wb.worksheets), 3)
+        self.assertIn("Raster", self.out_wb.sheetnames)
+        self.assertIn("Services", self.out_wb.sheetnames)
+        self.assertIn("Ressources", self.out_wb.sheetnames)
+        self.assertNotIn("Vecteurs", self.out_wb.sheetnames)
+        self.assertNotIn("Attributs", self.out_wb.sheetnames)
+
+    def test_output_structure_noRaster_attributes(self):
+        """Test output workbook worksheets with all types tags."""
+        # load tags fixtures
+        with open(self.tags_sample_types_noRaster, "r") as f:
+            search = json.loads(f.read())
+        # run
+        self.out_wb.set_worksheets(
+            auto=search.get("tags").keys(), attributes=1)
+        self.assertEqual(len(self.out_wb.worksheets), 4)
+        self.assertNotIn("Raster", self.out_wb.sheetnames)
+        self.assertIn("Services", self.out_wb.sheetnames)
+        self.assertIn("Ressources", self.out_wb.sheetnames)
+        self.assertIn("Vecteurs", self.out_wb.sheetnames)
+        self.assertIn("Attributs", self.out_wb.sheetnames)
+
+    def test_output_structure_noService(self):
+        """Test output workbook worksheets with all types tags."""
+        # load tags fixtures
+        with open(self.tags_sample_types_noService, "r") as f:
+            search = json.loads(f.read())
+        # run
+        self.out_wb.set_worksheets(
+            auto=search.get("tags").keys())
+        self.assertEqual(len(self.out_wb.worksheets), 3)
+        self.assertIn("Raster", self.out_wb.sheetnames)
+        self.assertNotIn("Services", self.out_wb.sheetnames)
+        self.assertIn("Ressources", self.out_wb.sheetnames)
+        self.assertIn("Vecteurs", self.out_wb.sheetnames)
+
+    def test_output_structure_noResource(self):
+        """Test output workbook worksheets with all types tags."""
+        # load tags fixtures
+        with open(self.tags_sample_types_noResource, "r") as f:
+            search = json.loads(f.read())
+        # run
+        self.out_wb.set_worksheets(
+            auto=search.get("tags").keys())
+        self.assertEqual(len(self.out_wb.worksheets), 3)
+        self.assertIn("Raster", self.out_wb.sheetnames)
+        self.assertIn("Services", self.out_wb.sheetnames)
+        self.assertNotIn("Ressources", self.out_wb.sheetnames)
+        self.assertIn("Vecteurs", self.out_wb.sheetnames)
+
+    def test_output_structure_bad(self):
+        """Test output workbook worksheets with all types tags."""
+        # run
+        with self.assertRaises(TypeError):
+            self.out_wb.set_worksheets(auto=["type:resource", "type:service"])
+
+    # -- Export ---------------------------------------------------------------
+    def test_metadata_export(self):
+        """Test search results export"""
+        # temp output file
+        out_xlsx = mkstemp(prefix="i2o_test_xlsx_")
+        # load tags fixtures
+        with open(self.search_all_includes, "r") as f:
+            search = json.loads(f.read())
+        # add worksheets
+        self.out_wb.set_worksheets(auto=search.get('tags').keys())
+        # run
+        for md in search.get('results'):
+            self.out_wb.store_metadatas(md)
+        # save
+        self.out_wb.save(out_xlsx[1] + ".xlsx")
+        print(out_xlsx[1] + ".xlsx")
+
+    def test_metadata_bad_no_dict(self):
+        """Test metadata not as a dict."""
+        # run
+        with self.assertRaises(TypeError):
+            self.out_wb.store_metadatas(["_id", "azertyqwerty",
+                                         "title", "fixturing me", ])
+
+    # def test_metadata_bad_type(self):
+    #     """Test bad metadata type."""
+    #     # run
+    #     with self.assertRaises(TypeError):
+    #         self.out_wb.store_metadatas({"_id": "azertyqwerty",
+    #                                      "type": "datasetEtMatch"})
+
+# #############################################################################
 # ##### Stand alone program ########
 # ##################################
 if __name__ == '__main__':
