930bc06fba5a78af21233783419c36f99dd61f57
Guts <Guts@users.noreply.github.com>
2018-08-14 19:51:39 +0200

    handling independent start and i18n #19

--- __main__.py
+++ __main__.py
@@ -13,11 +13,12 @@
     Updated:      22/08/2018
 """
 
-from PyQt5 import QtCore, QtGui, QtWidgets
 import qdarkstyle
 
-# modules - UI
-from modules.ui.export.ui_export import Ui_TabWidget
+# submodules - UI
+from modules.ui.auth.ui_authentication import Ui_dlg_authentication
+from modules.ui.credits.ui_credits import Ui_dlg_credits
+from modules.ui.main.ui_IsogeoToOffice import Ui_tabs_IsogeoToOffice
 
 # modules - functional
 from modules import Isogeo2xlsx
@@ -26,15 +27,22 @@ from modules import IsogeoStats
 from modules import isogeo2office_utils
 
 
+# #############################################################################
+# ##### Stand alone program ########
+# ##################################
 if __name__ == "__main__":
     import sys
     # create the application and the main window
     app = QtWidgets.QApplication(sys.argv)
-    TabWidget = QtWidgets.QTabWidget()
-    ui = Ui_TabWidget()
-    ui.setupUi(TabWidget)
-    # setup stylesheet
+    # apply dark style
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-    # run
-    TabWidget.show()
+    # apply language
+    locale_path = path.join(app_dir,
+                            'i18n',
+                            'IsogeoToOffice_{}.qm'.format(current_locale.system().name()))
+    translator = QtCore.QTranslator()
+    translator.load(path.realpath(locale_path))
+    app.installTranslator(translator)
+    # link to Isogeo to Office main UI
+    i2o = IsogeoToOffice_Main()
     sys.exit(app.exec_())
