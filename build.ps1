e2b6ba87d56722863a9b654cee30cc16f1fd00d3
Guts <Guts@users.noreply.github.com>
2018-08-17 19:53:38 +0200

    create required empty folders

--- build.ps1
+++ build.ps1
@@ -12,7 +12,7 @@ py -3 -m venv env3_packaging
 "`n-- STEP -- Install dependencies within the virtualenv and get UPX"
 python -m pip install -U pip
 pip install --upgrade -r ./requirements.txt
-pip install --upgrade -r ./requirements_dev.txt
+#pip install --upgrade -r ./requirements_dev.txt
 
 "`n-- STEP -- Update and compile UI"
 .\ui_compile.ps1
@@ -20,6 +20,10 @@ pip install --upgrade -r ./requirements_dev.txt
 "`n-- STEP -- Build and bundle forcing clean"
 rm -r dist\*
 pyinstaller -y --clean bundle_isogeo2office.spec
+
+"`n-- STEP -- Add required empty folders"
+new-item -Name "dist/isogeo2office/_auth" -ItemType directory 
+new-item -Name "dist/isogeo2office/_logs" -ItemType directory 
 
 "`n-- STEP -- Zipping"
 Add-Type -assembly "system.io.compression.filesystem"
