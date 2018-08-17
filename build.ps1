ee278bcfb8d9628a93fccef1818ed8ec485aa5eb
Guts <Guts@users.noreply.github.com>
2018-08-17 15:19:15 +0200

    add build tasks in VSC system

--- build.ps1
+++ build.ps1
@@ -14,26 +14,18 @@ python -m pip install -U pip
 pip install --upgrade -r ./requirements.txt
 pip install --upgrade -r ./requirements_dev.txt
 
-"`n-- STEP -- Update UI translations"
-pylupdate5 -noobsolete -verbose .\isogeo2office.pro
-
-"`n-- STEP -- Compile UI resources"
-pyrcc5 ".\resources.qrc" -o ".\resources_rc.py"
-
-"`n-- STEP -- Update UI elements"
-pyuic5 -x .\modules\ui\auth\ui_authentication.ui -o .\modules\ui\auth\ui_authentication.py
-pyuic5 -x .\modules\ui\credits\ui_credits.ui -o .\modules\ui\credits\ui_credits.py
-pyuic5 -x .\modules\ui\main\ui_IsogeoToOffice.ui -o .\modules\ui\main\ui_IsogeoToOffice.py
+"`n-- STEP -- Update and compile UI"
+.\ui_compile.ps1
 
 "`n-- STEP -- Build and bundle forcing clean"
 rm -r dist\*
 pyinstaller -y --clean bundle_isogeo2office.spec
 
 "`n-- STEP -- Zipping"
-# Add-Type -assembly "system.io.compression.filesystem"
-# $source=Join-Path (pwd) dist\isogeo2office
-# $dest=Join-Path (pwd) dist\i2o.zip
-# [io.compression.zipfile]::CreateFromDirectory($Source, $dest)
+Add-Type -assembly "system.io.compression.filesystem"
+$source=Join-Path (pwd) dist\isogeo2office
+$dest=Join-Path (pwd) dist\i2o.zip
+[io.compression.zipfile]::CreateFromDirectory($Source, $dest)
 
 "`n-- STEP -- Get out the virtualenv and cleanup"
 deactivate
