66db3493a4abbb32f864653307fcdc618321b5fe
Guts <Guts@users.noreply.github.com>
2018-09-04 10:27:23 +0200

    distinguish dev helpers and tests

---
+++ tests/development/dev_pyqt5_autocompletion.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
+
+app = QtWidgets.QApplication(sys.argv)
+
+model = QtCore.QStringListModel()
+model.setStringList(['some', 'words', 'in', 'my', 'dictionary', "ploup", "dinausaurus", "chokobons"])
+
+completer = QtWidgets.QCompleter()
+completer.setModel(model)
+
+lineedit = QtWidgets.QLineEdit()
+lineedit.setCompleter(completer)
+lineedit.show()
+
+sys.exit(app.exec_())
