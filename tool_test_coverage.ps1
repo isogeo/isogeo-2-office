70235af4b8c98f1b1aa43749db13c46f0e5aafd0
Guts <Guts@users.noreply.github.com>
2019-07-05 17:59:37 +0200

    remove pycodestyle

--- tool_test_coverage.ps1
+++ tool_test_coverage.ps1
@@ -14,20 +14,6 @@ pip install --upgrade setuptools wheel
 pip install --upgrade -r .\requirements.txt
 pip install --upgrade -r .\tests\requirements_test.txt
 
-"-- STEP -- Python code style"
-# main
-pycodestyle .\IsogeoToOffice.py --ignore="E265,E501" --statistics --show-source
-# threads
-pycodestyle modules/threads.py --ignore="E265,E501" --statistics --show-source
-# export modules
-pycodestyle modules/export/isogeo2docx.py --ignore="E265,E501" --statistics --show-source
-pycodestyle modules/export/isogeo2xlsx.py --ignore="E265,E501" --statistics --show-source
-pycodestyle modules/export/isogeo_stats.py --ignore="E265,E501" --statistics --show-source
-pycodestyle modules/export/formatter.py --ignore="E265,E501" --statistics --show-source
-# utils
-pycodestyle modules/utils/api.py --ignore="E265,E501" --statistics --show-source
-pycodestyle modules/utils/utils.py --ignore="E265,E501" --statistics --show-source
-
 "-- STEP -- Fixturing"
 python .\tests\fixturing.py
 
