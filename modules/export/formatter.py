085ba06105b24a2fcb15241f2f78c65972f5d872
Guts <Guts@users.noreply.github.com>
2019-08-23 18:51:14 +0200

    migrating formatter to sdk v3

--- modules/export/formatter.py
+++ modules/export/formatter.py
@@ -18,23 +18,25 @@
 # ##################################
 
 # Standard library
-from datetime import datetime
-from itertools import zip_longest
 import logging
 import re
+from itertools import zip_longest
 from urllib.parse import urlparse
 from xml.sax.saxutils import escape
 
 # 3rd party library
 import arrow
-from isogeo_pysdk import IsogeoTranslator
+from isogeo_pysdk import IsogeoTranslator, License, Limitation, Specification
 
+# custom submodules
+from modules.utils import isogeo2office_utils
 
 # ##############################################################################
 # ############ Globals ############
 # #################################
 
 logger = logging.getLogger("isogeo2office")  # LOG
+utils = isogeo2office_utils()
 
 # ##############################################################################
 # ########## Classes ###############
@@ -42,7 +44,17 @@ logger = logging.getLogger("isogeo2office")  # LOG
 
 
 class IsogeoFormatter(object):
-    """IsogeoFormatter class."""
+    """Metadata formatter to avoid repeat operations on metadata during export in different formats.
+    
+    :param str lang: selected language
+    :param str output_type: name of output type to format for. Defaults to 'Excel'
+    :param tuple default_values: values used to replace missing values. Structure:
+        
+        (
+            str_for_missing_strings_and_integers,
+            str_for_missing_dates
+        )
+    """
 
     def __init__(
         self,
@@ -50,20 +62,9 @@ class IsogeoFormatter(object):
         output_type="Excel",
         default_values=("NR", "1970-01-01T00:00:00+00:00"),
     ):
-        """Metadata formatter to avoid repeat oeprations on metadata.
-
-        :param str lang: selected language
-        :param str output_type: name of output type to format for
-        :param tuple default_values: values used to replace missing values.
-         2 values structure:
-            (
-            str_for_missing_strings_and_integers,
-            str_for_missing_dates
-            )
-        """
         # locale
-        self.lang = lang
-        if lang.lower() == "fr":
+        self.lang = lang.lower()
+        if lang == "fr":
             self.dates_fmt = "DD/MM/YYYY"
             self.locale_fmt = "fr_FR"
         else:
@@ -71,7 +72,7 @@ class IsogeoFormatter(object):
             self.locale_fmt = "uk_UK"
 
         # store params and imports as attributes
-        self.output_type = output_type
+        self.output_type = output_type.lower()
         self.defs = default_values
         self.isogeo_tr = IsogeoTranslator(lang).tr
 
@@ -85,11 +86,14 @@ class IsogeoFormatter(object):
         for c_in in md_cgus:
             cgu = {}
             # ensure other fields
-            cgu["description"] = self.clean_xml(c_in.get("description", ""))
-            if "license" in c_in.keys():
-                cgu["name"] = self.clean_xml(c_in.get("license").get("name", "NR"))
-                cgu["link"] = c_in.get("license").get("link", "")
-                cgu["content"] = self.clean_xml(c_in.get("license").get("content", ""))
+            if isinstance(c_in, str):
+                print(c_in)
+            cgu["description"] = utils.clean_xml(c_in.get("description", ""))
+            if "license" in c_in:
+                lic = License(**c_in.get("license"))
+                cgu["name"] = utils.clean_xml(lic.name)
+                cgu["link"] = lic.link
+                cgu["content"] = utils.clean_xml(lic.content)
             else:
                 cgu["name"] = self.isogeo_tr("conditions", "noLicense")
 
@@ -114,7 +118,7 @@ class IsogeoFormatter(object):
         for l_in in md_limitations:
             limitation = {}
             # ensure other fields
-            limitation["description"] = self.clean_xml(l_in.get("description", ""))
+            limitation["description"] = utils.clean_xml(l_in.get("description", ""))
             limitation["type"] = self.isogeo_tr("limitations", l_in.get("type"))
             # legal type
             if l_in.get("type") == "legal":
@@ -125,10 +129,10 @@ class IsogeoFormatter(object):
                 pass
             # INSPIRE precision
             if "directive" in l_in.keys():
-                limitation["inspire"] = self.clean_xml(
+                limitation["inspire"] = utils.clean_xml(
                     l_in.get("directive").get("name")
                 )
-                limitation["content"] = self.clean_xml(
+                limitation["content"] = utils.clean_xml(
                     l_in.get("directive").get("description")
                 )
             else:
@@ -154,85 +158,35 @@ class IsogeoFormatter(object):
         """
         specs_out = []
         for s_in in md_specifications:
-            spec = {}
+            spec_in = Specification(**s_in.get("specification"))
+            spec_out = {}
             # translate specification conformity
             if s_in.get("conformant"):
-                spec["conformity"] = self.isogeo_tr("quality", "isConform")
+                spec_out["conformity"] = self.isogeo_tr("quality", "isConform")
             else:
-                spec["conformity"] = self.isogeo_tr("quality", "isNotConform")
+                spec_out["conformity"] = self.isogeo_tr("quality", "isNotConform")
             # ensure other fields
-            spec["name"] = s_in.get("specification").get("name")
-            spec["link"] = s_in.get("specification").get("link", "")
+            spec_out["name"] = spec_in.name
+            spec_out["link"] = spec_in.link
             # make data human readable
-            try:
-                spec_date = arrow.get(s_in.get("specification").get("published")[:19])
-                spec_date = "{0}".format(
-                    spec_date.format(self.dates_fmt, self.locale_fmt)
-                )
-            except TypeError:
-                logger.warning(
-                    "Publication date is missing in the "
-                    "specification '{} ({})'. Specifications should"
-                    " have a publication date.".format(
-                        spec.get("name"), s_in.get("specification").get("_tag")
-                    )
-                )
+            if spec_in.published:
+                spec_date = utils.hlpr_datetimes(spec_in.published).strftime(self.dates_fmt)
+            else:
+                logger.warning("Publication date is missing in the specification '{} ({})'".format(spec_in.name, spec_in._tag))
                 spec_date = ""
-            spec["date"] = spec_date
+            spec_out["date"] = spec_date
             # store into the final list
             specs_out.append(
                 "{} {} {} - {}".format(
-                    spec.get("name"),
-                    spec.get("date"),
-                    spec.get("link"),
-                    spec.get("conformity"),
+                    spec_out.get("name"),
+                    spec_out.get("date"),
+                    spec_out.get("link"),
+                    spec_out.get("conformity"),
                 )
             )
 
         # return formatted result
         return specs_out
-
-    # ------------ Prevent encoding errors ------------------------------------
-    def remove_accents(self, input_str, substitute=""):
-        """Clean string from special characters.
-
-        source: http://stackoverflow.com/a/5843560
-        """
-        return unicode(substitute).join(char for char in input_str if char.isalnum())
-
-    def clean_xml(self, invalid_xml, mode="soft", substitute="_"):
-        """Clean string of XML invalid characters.
-
-        source: http://stackoverflow.com/a/13322581/2556577
-        """
-        # assumptions:
-        #   doc = *( start_tag / end_tag / text )
-        #   start_tag = '<' name *attr [ '/' ] '>'
-        #   end_tag = '<' '/' name '>'
-        ws = r"[ \t\r\n]*"  # allow ws between any token
-        name = "[a-zA-Z]+"  # note: expand if necessary but the stricter the better
-        attr = '{name} {ws} = {ws} "[^"]*"'  # note: fragile against missing '"'; no "'"
-        start_tag = "< {ws} {name} {ws} (?:{attr} {ws})* /? {ws} >"
-        end_tag = "{ws}".join(["<", "/", "{name}", ">"])
-        tag = "{start_tag} | {end_tag}"
-
-        assert "{{" not in tag
-        while "{" in tag:  # unwrap definitions
-            tag = tag.format(**vars())
-
-        tag_regex = re.compile("(%s)" % tag, flags=re.VERBOSE)
-
-        # escape &, <, > in the text
-        iters = [iter(tag_regex.split(invalid_xml))] * 2
-        pairs = zip_longest(*iters, fillvalue="")  # iterate 2 items at a time
-
-        # get the clean version
-        clean_version = "".join(escape(text) + tag for text, tag in pairs)
-        if mode == "strict":
-            clean_version = re.sub(r"<.*?>", substitute, clean_version)
-        else:
-            pass
-        return clean_version
 
     # ------------ Hyperlinks -------------------------------------------------
     def url_edit(self, input_link: str, output_type: str = "xlsx"):
