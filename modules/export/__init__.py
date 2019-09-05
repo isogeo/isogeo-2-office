4c26ddb8f01ba88034cc4cc061c52e9f27631832
Guts <Guts@users.noreply.github.com>
2019-09-05 11:44:44 +0200

    remove code for dox export

--- modules/export/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#! python3
-
-# export helpers
-from .formatter import IsogeoFormatter  # noqa: F401
-from .isogeo_stats import IsogeoStats  # noqa: F401
-
-# export modules
-from .isogeo2docx import Isogeo2docx  # noqa: F401
