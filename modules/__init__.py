5d94e4c8375b563c2bef74be46ada25250d2c153
Guts <Guts@users.noreply.github.com>
2019-09-13 15:53:09 +0200

    flake8

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 #! python3  # noqa: E265
 
-from .ui import *
-from .utils import *
-from .threads import *
+from .ui import *  # noqa: F401
+from .utils import *  # noqa: F401
+from .threads import *  # noqa: F401
