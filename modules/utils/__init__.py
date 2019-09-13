6c6c94f478333565c0f6688e3b16b63f608595b3
Guts <Guts@users.noreply.github.com>
2019-09-13 14:44:47 +0200

    fix flake8

--- modules/utils/__init__.py
+++ modules/utils/__init__.py
@@ -1,2 +1,5 @@
-from .utils import isogeo2office_utils
-from .api import IsogeoApiMngr
+# coding: utf-8
+#! python3  # noqa: E265
+
+from .utils import isogeo2office_utils  # noqa: F401
+from .api import IsogeoApiMngr  # noqa: F401
