3665071db6da5ade283c787678c130bc6d1707cd
Guts <Guts@users.noreply.github.com>
2020-01-20 11:33:55 +0100

    Cleaning up

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,7 +22,6 @@ import xml.etree.ElementTree as ET
 
 # module target
 from modules import isogeo2office_utils
-from IsogeoToOffice import *
 
 # #############################################################################
 # ########## Classes ###############
